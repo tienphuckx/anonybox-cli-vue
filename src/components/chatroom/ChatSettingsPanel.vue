7d40baa88a9b196993eda6c048f0bae7e0b17ae0
tienphuckx <tienphuckx@gmail.com>
2024-11-06 13:56:16 +0700

    impr: display joined member in group setting panel

--- src/components/chatroom/ChatSettingsPanel.vue
+++ src/components/chatroom/ChatSettingsPanel.vue
@@ -14,15 +14,40 @@
           </div>
         </div>
   
-        <!-- Joined Members List -->
         <div v-if="groupInfo.list_joined_member.length" class="border-t pt-4 space-y-2">
-          <h4 class="text-lg font-semibold">Members</h4>
-          <ul class="list-disc pl-6">
-            <li v-for="member in groupInfo.list_joined_member" :key="member.user_id">
-              {{ member.username }}
+        <h4 class="text-lg font-semibold">Joined Members</h4>
+        <ul class="list-disc pl-6 space-y-2">
+            <li
+            v-for="member in visibleMembers"
+            :key="member.user_id"
+            class="grid grid-cols-2 items-center gap-4"
+            >
+            <!-- Member Name in First Column -->
+            <span>{{ member.username }}</span>
+            
+            <!-- Actions in Second Column (Only visible if the user is the group owner) -->
+            <span v-if="isGroupOwner" class="flex space-x-4 justify-end">
+                <button @click="removeMember(member.user_id)" class="text-red-500" title="Remove Member">
+                <i class="fas fa-user-slash"></i>
+                </button>
+                <button @click="muteMember(member.user_id)" class="text-yellow-500" title="Mute Member">
+                <i class="fas fa-volume-mute"></i>
+                </button>
+            </span>
             </li>
-          </ul>
+        </ul>
+        
+        <div class="flex justify-end">
+            <button
+            v-if="!showAllMembers && groupInfo.list_joined_member.length > 5"
+            @click="showAllMembers = true"
+            class="text-blue-500 mt-2"
+            >
+            See All Members
+            </button>
         </div>
+        </div>
+
   
         <!-- Waiting Members List -->
         <div v-if="groupInfo.total_waiting_member > 0" class="border-t pt-4 space-y-2">
@@ -69,8 +94,21 @@
         required: true
       }
     },
+    computed: {
+        visibleMembers() {
+        // Show only the first 5 members if showAllMembers is false
+        return this.showAllMembers
+            ? this.groupInfo.list_joined_member
+            : this.groupInfo.list_joined_member.slice(0, 5);
+        }, 
+        isGroupOwner() {
+            const userId = localStorage.getItem("x-user-id");
+            return parseInt(userId, 10) === this.groupInfo.owner_id;
+        }
+    },
     data() {
       return {
+        showAllMembers: false,
         groupInfo: {
           group_name: '',
           total_joined_member: 0,
@@ -84,9 +122,17 @@
       await this.fetchGroupDetails();
     },
     methods: {
-      async fetchGroupDetails() {
+        removeMember(userId) {
+            console.log(`Removing member with ID: ${userId}`);
+            // Implement API call here to remove the member
+        },
+        muteMember(userId) {
+            console.log(`Muting member with ID: ${userId}`);
+            // Implement API call here to mute the member
+        },
+        async fetchGroupDetails() {
         try {
-            // const url = `http://localhost:8080/group-detail/setting/${this.groupId}/${this.userId}`;
+            // const userId = localStorage.getItem("x-user-id");
             const url = `http://localhost:8080/group-detail/setting/${this.groupId}`;
 
             console.log(url);
